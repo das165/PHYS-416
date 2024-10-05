20b5bfeeb98f2f8b753ae62de32fcab0e98884e9
das165 <143668563+das165@users.noreply.github.com>
2024-10-04 20:41:46 -0400

    Update PHYS_416_CT.py

--- PHYS_416_CT.py
+++ PHYS_416_CT.py
@@ -37,13 +37,9 @@ from skimage.transform import iradon
 #reconstruction 
 
 reconstruction_fbp = iradon(projection, theta=theta, filter_name='cosine')
-error = reconstruction_fbp - image
-print(f'FBP rms reconstruction error: {np.sqrt(np.mean(error**2)):.3g}')
 
 imkwargs = dict(vmin=-0.2, vmax=0.2)
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4.5), sharex=True, sharey=True)
+fig, ax1 = plt.subplots(figsize=(8, 4.5))
 ax1.set_title("Reconstruction\nFiltered back projection")
 ax1.imshow(reconstruction_fbp, cmap=plt.cm.Greys_r)
-ax2.set_title("Reconstruction error\nFiltered back projection")
-ax2.imshow(reconstruction_fbp - image, cmap=plt.cm.Greys_r, **imkwargs)
 plt.show()
